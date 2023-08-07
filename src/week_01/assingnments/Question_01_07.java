493e05b2f565e8212fb2745b0d40fb1f0298d42e
Sinan CETIN <sinanctn54@gmai.com>
2023-08-07 09:18:12 -0500

    added the solution code for Week_01 - Question_01_07

---
+++ src/week_01/assingnments/Question_01_07.java
@@ -0,0 +1,10 @@
+package week_01.assingnments;
+
+public class Question_01_07 {
+    public static void main(String[] args) {
+        System.out.println("π1 =");
+        System.out.println(4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+        System.out.println("π2 =");
+        System.out.println(4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13)));
+    }
+}
