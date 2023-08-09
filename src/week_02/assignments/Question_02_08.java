19b3f0c0f9bfc87e7bd5048df5ddd33052cee5f4
AhmetFurkanCann <afc06180@gmail.com>
2023-08-10 01:19:23 +0300

    added the solution code for Week_02 - Question_02_08

---
+++ src/week_02/assignments/Question_02_08.java
@@ -0,0 +1,39 @@
+package week_02.assignments;
+
+import java.util.Scanner;
+
+public class Question_02_08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user to enter the time offset of GMT
+        System.out.print("Enter the time zone offset to GMT: ");
+        int offset = input.nextInt();
+
+        // Obtain the total milliseconds since midnight, Jan 1, 1970
+        long totalMilliseconds = System.currentTimeMillis();
+
+        // Obtain the total seconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000;
+
+        // Compute the current second in the minute in the hour
+        long currentSecond = totalSeconds % 60;
+
+        // Obtain the total minutes
+        long totalMinutes = totalSeconds / 60;
+
+        // Compute the current minute in the hour
+        long currentMinute = totalMinutes % 60;
+
+        // Obtain the total hours
+        long totalHours = totalMinutes / 60;
+
+        // Compute the current hour
+        long currentHour = totalHours % 24;
+        currentHour = currentHour + offset;
+
+        // Display results
+        System.out.println("Current time is " + currentHour + ":"
+                + currentMinute + ":" + currentSecond);
+    }
+}
