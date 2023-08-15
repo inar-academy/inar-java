9d2a41b6346e1e36065ca690a0b071b6519d227e
AhmetFurkanCann <afc06180@gmail.com>
2023-08-15 18:48:08 +0300

    Code Fix in Week_02 - Question_02_13

--- src/week_02/assignments/Question_02_13.java
+++ src/week_02/assignments/Question_02_13.java
@@ -15,17 +15,17 @@ public class Question_02_13 {
         double savingAmount = input.nextDouble();
 
         // Compute first month account value
-        double total = 100 * (1 + MONTHLY_INTEREST_RATE);
+        double total = savingAmount * (1 + MONTHLY_INTEREST_RATE);
         // Compute second month account value
-        total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+        total = (savingAmount + total) * (1 + MONTHLY_INTEREST_RATE);
         // Compute third month account value
-        total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+        total = (savingAmount + total) * (1 + MONTHLY_INTEREST_RATE);
         // Compute forth month account value
-        total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+        total = (savingAmount + total) * (1 + MONTHLY_INTEREST_RATE);
         // Compute fifth month account value
-        total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+        total = (savingAmount + total) * (1 + MONTHLY_INTEREST_RATE);
         // Compute sixth month account value
-        total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+        total = (savingAmount + total) * (1 + MONTHLY_INTEREST_RATE);
 
         // Display result
         System.out.println("After the sixth month, the account value is " + total);
