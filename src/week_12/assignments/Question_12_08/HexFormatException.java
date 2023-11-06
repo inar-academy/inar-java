24ff90cac2bd759aa8f1b923b969f54ec31ac8b1
Uzeyir Sur <uzeyirsure@gmail.com>
2023-11-06 16:01:23 +0300

    until week_10 solutions

--- src/week_12/assignments/Question_12_08/HexFormatException.java
+++
@@ -1,7 +0,0 @@
-package week_12.assignments.Question_12_08;
-
-public class HexFormatException extends NumberFormatException{
-    public HexFormatException(String message){
-        super(message);
-    }
-}
--- src/week_12/assignments/Question_12_09/BinaryFormatException.java
+++
@@ -1,7 +0,0 @@
-package week_12.assignments.Question_12_09;
-
-public class BinaryFormatException extends Exception {
-    public BinaryFormatException(String message) {
-        super(message);
-    }
-}
